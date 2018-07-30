56609d9c3446c50da797f6f38df67e527193489d
Patrick Gendotti <patrick.gendotti@gmail.com>
2018-07-30 13:06:16 -0700

    Update so that Cat Clicker is fully functional! Hit a snag trying to update the currentCat -> needed to pass in the other Cat objects as an input for the self.currentCat(...) property, not redefine that property as self.currentCat() = ... (see KO documentation here: http://knockoutjs.com/documentation/observables.html to understand). Good to know, and still did not take too long!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,44 @@
 
+var Tabby = {
+  clickCount: 0,
+  name: "Tabby",
+  imgSrc: "img/434164568_fea0ad4013_z.jpg",
+  nicknames: ["Tab-Tab", "T-Bone", "Mr. T", "Mr. Teabody"]
+}
+
+var Sleepy = {
+  clickCount: 0,
+  name: "Sleepy",
+  imgSrc: "img/9648464288_2516b35537_z.jpg",
+  nicknames: ["Dopey", "Lazy"]
+}
+
+var Shadow = {
+  clickCount: 0,
+  name: "Shadow",
+  imgSrc: "img/1413379559_412a540d29_z.jpg",
+  nicknames: ["Shady", "Darko"]
+}
+
+var Tiger = {
+  clickCount: 0,
+  name: "Tiger",
+  imgSrc: "img/4154543904_6e2428c421_z.jpg",
+  nicknames: ["Tigger", "Tignificent"]
+}
+
+var Smalley = {
+  clickCount: 0,
+  name: "Smalley",
+  imgSrc: "img/22252709_010df3379e_z.jpg",
+  nicknames: ["Snow", "Kiddie Kitty"]
+}
+
+catList = [Tabby, Sleepy, Shadow, Tiger, Smalley];
+
+
+
+
 var Cat = function(data) {
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -28,15 +68,24 @@ var Cat = function(data) {
 var ViewModel = function() {
   var self = this;
 
-  this.currentCat = ko.observable(new Cat({
-    clickCount: 0,
-    name: "Tabby",
-    imgSrc: "img/434164568_fea0ad4013_z.jpg",
-    nicknames: ["Tab-Tab", "T-Bone", "Mr. T", "Mr. Teabody"]
-  }));
+  var newCats = [];
+
+  catList.forEach(function(item){
+    var this_cat = new Cat(item);
+    newCats.push(this_cat);
+  });
+
+  self.allCats = ko.observableArray(newCats),
+
+  self.currentCat = ko.observable(self.allCats()[0]), //set to the first cat in the cat list
 
   this.incrementCounter = function() {
     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+
+  this.setCurrentCat = function() {
+    var index = self.allCats.indexOf(this);
+    self.currentCat(self.allCats()[index]);
   };
 }
 
