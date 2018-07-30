55a0f4c687519ba105f0dadd26c179afc0bf294c
Patrick Gendotti <patrick.gendotti@gmail.com>
2018-07-30 10:21:28 -0700

    Now functional with a Cat object added to the ViewModel. Still need to upgrade to load a list of different cats!

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-var ViewModel = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable("Tabby");
-  this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
-  this.imgAttribution = ko.observable("");
 
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable("");
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.catLevel = ko.computed(function() {
     if(this.clickCount()<10) {
@@ -25,6 +23,21 @@ var ViewModel = function() {
       return "Senior";
     }
   }, this);
+}
+
+var ViewModel = function() {
+  var self = this;
+
+  this.currentCat = ko.observable(new Cat({
+    clickCount: 0,
+    name: "Tabby",
+    imgSrc: "img/434164568_fea0ad4013_z.jpg",
+    nicknames: ["Tab-Tab", "T-Bone", "Mr. T", "Mr. Teabody"]
+  }));
+
+  this.incrementCounter = function() {
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
 }
 
 ko.applyBindings(new ViewModel())
